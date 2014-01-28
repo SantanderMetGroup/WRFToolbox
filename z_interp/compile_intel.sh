e6b26a0ae2bc5d03c03cb91e9917812de143b549
Markel Garcia <markelg@users.noreply.github.com>
2014-01-28 16:36:18 +0000

    Adding z_interp 2
    git-svn-id: https://meteo.unican.es/svn/repos/WRF4G/trunk/WRFtoolbox@2004 3232fcf4-bb0b-194b-8ba9-96a9312bd5d0

---
+++ z_interp/compile_intel.sh
@@ -0,0 +1,4 @@
+use intel
+export NETCDFDIR="/software/meteo/netcdf/netcdf_4.1.3_intel11_netcdf4"
+export HDF5DIR="/software/meteo/hdf5/hdf5_1.8.8_intel11"
+ifort -O3 -heap-arrays  z_interp.F90 -o z_interp -I$NETCDFDIR/include -I$HDF5DIR/include -Bstatic -L$NETCDFDIR/lib -lnetcdf -lnetcdff -L$HDF5DIR/lib -lnetcdf -lhdf5_hl -lhdf5 -lz >& compile.log
