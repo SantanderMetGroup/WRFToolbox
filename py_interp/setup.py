e9b85a14152835c7f4cdd57efb75ec4d540bc7f3
Alvaro Lavin <lavina@unican.es>
2018-03-14 14:01:13 +0100

    Merging with gitlab repository

--- py_interp/setup.py
+++ py_interp/setup.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 
-import os.path
 from numpy.distutils.core import setup, Extension
 
 setup(
     name='py_interp',
     platforms=['GNU/Linux'],
-    version='1.2.0',
+    version='1.3.0',
     author='Marker Garcia',
     author_email='markel.garcia@ic3.cat',
     description=( 'It is a command line tool for interpolating WRF output files to pressure levels, ' 
                    'as well as computing a series of diagnostics such as low, medium and high cloud cover' ),
     keywords=['climate', 'weather'],
-    install_requires=[ 'numpy', 'netCDF4'],
+    install_requires=[ 'numpy', 'netCDF4', 'scipy'],
     url='https://github.com/markelg/py_interp',
     packages=['py_interp'],
     license='MIT',
@@ -26,6 +25,8 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 "],
     scripts=['bin/py_interp'], 
-    ext_modules = [Extension( 'py_interp_fortran', ['src/py_interp_fortran.F90'] )],
+    ext_modules = [Extension( 'py_interp_fortran', ['src/py_interp_fortran.F90']),]
 )
+
+
 
