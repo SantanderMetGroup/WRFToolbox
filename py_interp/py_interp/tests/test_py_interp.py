09917e696ac92deff714848a365bb173ce4e6f58
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-30 14:33:09 +0800

    Delete py_interp directory

--- py_interp/py_interp/tests/test_py_interp.py
+++
@@ -1,8 +0,0 @@
-import py_interp.mainfun as pm
-
-ifile = "/home/users/garciam/pruebas/py_interp/wrfout_d03_20161001120000.nc"
-varlist = ["T2", "TSK", "Q2","PSFC", "U10", "V10", "CLT", "TLAPSE200"]
-vars_to_filter = ["CLT",]
-plevs = [1000, 925, 850, 700, 500, 300]
-pm.py_interp_file(ifile, varlist, plevs, vars_to_filter, overwrite=False,
-                  verbose=True)
